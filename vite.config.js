722d4f3cf9488abf451890a3be58b8f08eea948d
prakashknaikade <prna00001@stud.uni-saarland.de>
2024-07-24 18:38:14 +0200

    Revert "Update vite.config.js"
    This reverts commit 255b82382fda8453ee5dbeb25ff13deaa3542735.

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/StructColorPaintingViewer/',
+  base: '/StructColorPainitingViewer/',
   build: {
     outDir: 'dist'
   }
