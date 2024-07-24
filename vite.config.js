255b82382fda8453ee5dbeb25ff13deaa3542735
Prakash Naikade <prakashknaikade@gmail.com>
2024-07-24 15:41:46 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/StructColorPainitingViewer/',
+  base: '/StructColorPaintingViewer/',
   build: {
     outDir: 'dist'
   }
